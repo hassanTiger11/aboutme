34ccc3b8d67a9f41f4fe7e244dd3a21922e72e6c
Hassan Alnamer <halnamer@email.arizona.edu>
2022-06-05 11:50:18 -0700

    changed repo name

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import TopBar from './TopBar';
 import Terminal from './Terminal';
+import { useEffect } from 'react';
 function App() {
+  useEffect(() => {
+    document.title = "Hassan Alnamer"
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
